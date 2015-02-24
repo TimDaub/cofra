25dfb16a9b80cc5615bc4c57e4a16e31e1f8dd0c
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2015-02-24 17:14:21 +0100

    fixed some text classes

--- tests/test_emotext.py
+++ tests/test_emotext.py
@@ -5,15 +5,22 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from apis.emotext import text_processing
 
 texts = [
-    "Hello. This is doge!/) All those punctuation chars should be removed.$%&"
+    "Hello. This is doge! All those punctuation chars should be removed."
 ]
 
 def test_text_processing():
+    """
+    Tests:
+        * if strings get separated accordingly
+        * words contain punctuation chars
+        * if there are sentences that have no words.
+        * the number of sentences
+    """
     sentences = text_processing(texts[0], stemming=False, remove_punctuation=True)
+    assert len(sentences) == 3
     for sentence in sentences:
         assert type(sentence) == type([])
         assert len(sentence) > 0
         for w in sentence:
             # check for punctuation characters in word sequence
             assert re.match(r'[^\w]', w) == None
-    print sentences
